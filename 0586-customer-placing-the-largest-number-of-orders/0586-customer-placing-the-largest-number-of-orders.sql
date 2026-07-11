35974eaeaac5159ed146ed678fa9900438dc013a
SATYAJIT NAYAK <satyajitnayak1312@gmail.com>
2026-07-11 23:45:38 +0530

    Time: 648 ms (8.08%), Space: 0B (100%) - LeetHub

---
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+SELECT customer_number
+FROM Orders
+GROUP BY customer_number
+HAVING COUNT(*) =
+(
+    SELECT MAX(order_count)
+    FROM
+    (
+        SELECT COUNT(*) AS order_count
+        FROM Orders
+        GROUP BY customer_number
+    ) t
+);
